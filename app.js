27becfe88ef0443d0ef9d2375bd8ecd0ae5844a9
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-07-11 13:03:06 +0200

    feat: route order confirmation implemented

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/jwRoutes.js'
-import errorHandler  from './middlewares/errorHandler.js';
-import notFound from './middlewares/notFound.js';
+import express from "express";
+import cors from "cors";
+import router from "./routes/jwRoutes.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import notFound from "./middlewares/notFound.js";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(express.static('public'));
-app.use(cors({
-    origin: process.env.FE_URL
-}))
-
-
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: process.env.FE_URL,
+  })
+);
 
 //rotta di debug
 
-app.get('/', (req,res) => {
-    res.json({message: "Benvenuto nella rotta base"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Benvenuto nella rotta base" });
+});
 
-app.use('/products', router)
+app.use("/products", router);
+app.use("/orders", router)
 
-
-
-app.use(notFound)
-app.use(errorHandler)
-app.listen(port, ()=>{
-    console.log('Server in ascolto sulla porta', port)
-})
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log("Server in ascolto sulla porta", port);
+});
